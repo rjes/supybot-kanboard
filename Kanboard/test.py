07b3c79c5dca4283762a977fab583614f45cebdf
Robert Soderlund <robban@netnerdz.se>
2019-01-06 13:19:18 +0100

    Added some other files

---
+++ Kanboard/test.py
@@ -0,0 +1,14 @@
+###
+# Copyright (c) 2019, Robert Soderlund
+# All rights reserved.
+#
+#
+###
+
+from supybot.test import *
+
+class KanboardTestCase(PluginTestCase):
+    plugins = ('Kanboard',)
+
+
+# vim:set shiftwidth=4 tabstop=4 expandtab textwidth=79:
